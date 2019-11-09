55864a94fa6e87f76da5b505e8c308cdb11d4fc8
Kiberfilin <kiberfilin@gmail.com>
2019-11-09 11:45:14 +0300

    Start making Place feature

---
+++ app/src/main/java/ru/cyber_eagle_owl/saddayappkt/clean/presentation/features/place/fragments/PlacesListingRecyclerViewAdapter.kt
@@ -0,0 +1,25 @@
+package ru.cyber_eagle_owl.saddayappkt.clean.presentation.features.place.fragments
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class PlacesListingRecyclerViewAdapter :
+    RecyclerView.Adapter<PlacesListingRecyclerViewAdapter.PlscesListingViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlscesListingViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: PlscesListingViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    class PlscesListingViewHolder(var view: View): RecyclerView.ViewHolder(view) {
+
+    }
+}
--- app/src/main/java/ru/cyber_eagle_owl/saddayappkt/clean/presentation/features/place/viper/placeslistingfragment/PlacesListingViewImpl.kt
+++ app/src/main/java/ru/cyber_eagle_owl/saddayappkt/clean/presentation/features/place/viper/placeslistingfragment/PlacesListingViewImpl.kt
@@ -1,5 +1,9 @@
 package ru.cyber_eagle_owl.saddayappkt.clean.presentation.features.place.viper.placeslistingfragment
 
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.RecyclerView
+import ru.cyber_eagle_owl.saddayappkt.R
+import ru.cyber_eagle_owl.saddayappkt.clean.presentation.features.place.fragments.PlacesListingRecyclerViewAdapter
 import ru.cyber_eagle_owl.saddayappkt.clean.presentation.features.place.viper.PlacesListingViperContract
 import ru.cyber_eagle_owl.saddayappkt.clean.presentation.vipercore.BaseView
 import ru.cyber_eagle_owl.saddayappkt.utils.wrappers.RouterToolbox
@@ -9,17 +13,35 @@ import javax.inject.Inject
 class PlacesListingViewImpl @Inject constructor() : BaseView<PlacesListingViperContract.PlacesListingPresenter>(),
     PlacesListingViperContract.PlacesListingView {
 
+    private lateinit var placesListingSwipeRefresh: SwipeRefreshLayout
+    private lateinit var placesListingRecyclerView: RecyclerView
+
+    private lateinit var placesListingAdapter: PlacesListingRecyclerViewAdapter
+
+    private lateinit var onRefreshListener: SwipeRefreshLayout.OnRefreshListener
+
     override fun onStart(toolbox: RouterToolbox) {
         Timber.d("onResume()")
         initViews()
+
+        val dens: Float = viperRootView.context.resources.displayMetrics.density
+        prepareRecyclerView(viperRootView.context.resources.configuration.orientation, dens)
+
         presenter.apply {
             onStart(toolbox)
             onViewCreated(this@PlacesListingViewImpl)
         }
     }
 
+    private fun prepareRecyclerView(orientation: Int, dens: Float) {
+        Timber.d("prepareRecyclerView()")
+        placesListingAdapter = PlacesListingRecyclerViewAdapter()
+
+        }
+
     private fun initViews() {
         Timber.d("initViews()")
-        //todo
+        placesListingSwipeRefresh = viperRootView.findViewById(R.id.placesListingSwipeRefresh)
+        placesListingRecyclerView = viperRootView.findViewById(R.id.placesListingRecyclerView)
     }
 }
